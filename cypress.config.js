d081be0ee6987952177c723bd0bd517e7c9497fd
Regina Becker <arquivosreginaarquivos@gmail.com>
2024-09-02 11:33:35 -0300

    Atualizando config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'Projeto api',
   e2e: {
-    baseUrl: 'https://api.restful-api.dev',
+    "baseUrl": "https://api.restful-api.dev/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
